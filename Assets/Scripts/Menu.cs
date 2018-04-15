812a412b8a61315589febfffb77a9221e24b1827
Cawotte <khamaz.ee@gmail.com>
2018-04-15 23:30:56 +0200

    Menu UI

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void playGame()
+    {
+		SceneManager.LoadScene (1);
+    }
+}
