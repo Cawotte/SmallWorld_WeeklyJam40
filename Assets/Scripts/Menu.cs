5b977b562d814abab1f61678ae19eb9100587b85
Cawotte <khamaz.ee@gmail.com>
2018-04-19 17:58:04 +0200

    almost final build

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -15,4 +15,5 @@ public class Menu : MonoBehaviour {
         Debug.Log("Game Quitted !");
         Application.Quit();
     }
+
 }
---
+++ Assets/Scripts/toVanille.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class toVanille : MonoBehaviour {
+
+    public RectTransform vanillePanel;
+    public TextMeshProUGUI vanilleText;
+
+    private void Start()
+    {
+        vanillePanel.gameObject.SetActive(false);
+        vanilleText.enabled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        vanillePanel.gameObject.SetActive(true);
+        vanilleText.enabled = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        vanillePanel.gameObject.SetActive(false);
+        vanilleText.enabled = false;
+    }
+}
